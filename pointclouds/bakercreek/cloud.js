308037c85e93e25c76d66586d9180281ff1b2836
Katie Nicolato <47072826+kitnixx@users.noreply.github.com>
2019-05-28 12:28:33 -0700

    Final commit

---
+++ pointclouds/bakercreek/cloud.js
@@ -0,0 +1,26 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 2462559,
+    "boundingBox": {
+        "lx": 474648.5,
+        "ly": 4942102.53,
+        "lz": -0.61,
+        "ux": 475293.4,
+        "uy": 4942747.430000001,
+        "uz": 644.2900000000233
+    },
+    "tightBoundingBox": {
+        "lx": 474648.5,
+        "ly": 4942102.53,
+        "lz": -0.61,
+        "ux": 475293.4,
+        "uy": 4942743.49,
+        "uz": 65.12
+    },
+    "pointAttributes": "LAS",
+    "spacing": 5.584997653961182,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
